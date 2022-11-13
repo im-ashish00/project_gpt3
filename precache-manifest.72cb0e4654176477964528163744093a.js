3e69cfd00ed1875ba2e953e9ad94a74d845bc1de
Ashish Sharma <iamashishsharma00@gmail.com>
2022-11-13 22:26:08 +0530

    Updates

---
+++ precache-manifest.72cb0e4654176477964528163744093a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "504e1ec4782c11456622",
+    "url": "/project_gpt3/static/css/main.5f2d05c4.chunk.css"
+  },
+  {
+    "revision": "504e1ec4782c11456622",
+    "url": "/project_gpt3/static/js/main.504e1ec4.chunk.js"
+  },
+  {
+    "revision": "610293909fd80949cbd5",
+    "url": "/project_gpt3/static/js/1.61029390.chunk.js"
+  },
+  {
+    "revision": "085bdfbe47dfd3e98e48",
+    "url": "/project_gpt3/static/js/runtime~main.085bdfbe.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/project_gpt3/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/project_gpt3/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/project_gpt3/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/project_gpt3/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/project_gpt3/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/project_gpt3/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/project_gpt3/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/project_gpt3/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/project_gpt3/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "2427cd2bbbdd3e61764844e0b633d341",
+    "url": "/project_gpt3/index.html"
+  }
+];
